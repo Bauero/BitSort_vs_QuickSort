2d15f48ff237f35c8af2f820f16562ebc885c5b0
Piotr Bauer <piotr.bauer.konto@gmail.com>
2025-11-05 22:25:26 +0100

    Make custom struct and cleanu array generation and freeing

--- main.c
+++ main.c
@@ -9,25 +9,69 @@
 #define RUNS 100               // liczba powtórzeń
 #define PARALLEL_THRESHOLD 100000
 #define SWAP(a, b) { elem_t tmp = (a); (a) = (b); (b) = tmp; }
-// ------------------------------------------------
 
+
+// ----------------------------- CUSTOM STRUCTURES -----------------------------
+typedef struct {
+    elem_t *arr_base;
+    elem_t *arr_bit_seq;
+    elem_t *arr_bit_par;
+    elem_t *arr_std_seq;
+    elem_t *arr_std_par;
+} array_list;
+
+// --------------------------------- FUNCTIONS ---------------------------------
 static inline double now_sec(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return ts.tv_sec + ts.tv_nsec * 1e-9;
 }
 
-int main(void) {
-    elem_t *arr_base    = malloc(ARRAY_SIZE * sizeof(elem_t));
-    elem_t *arr_bit_seq = malloc(ARRAY_SIZE * sizeof(elem_t));
-    elem_t *arr_bit_par = malloc(ARRAY_SIZE * sizeof(elem_t));
-    elem_t *arr_std_seq = malloc(ARRAY_SIZE * sizeof(elem_t));
-    elem_t *arr_std_par = malloc(ARRAY_SIZE * sizeof(elem_t));
+int create_arrays(array_list *arrays, int arr_size) {
+    arrays->arr_base    = malloc(arr_size * sizeof(elem_t));
+    arrays->arr_bit_seq = malloc(arr_size * sizeof(elem_t));
+    arrays->arr_bit_par = malloc(arr_size * sizeof(elem_t));
+    arrays->arr_std_seq = malloc(arr_size * sizeof(elem_t));
+    arrays->arr_std_par = malloc(arr_size * sizeof(elem_t));
     
-    if (!arr_base || !arr_bit_seq || !arr_bit_par || !arr_std_seq || !arr_std_par) {
+    if (!arrays->arr_base
+        || !arrays->arr_bit_seq || !arrays->arr_bit_par 
+        || !arrays->arr_std_seq || !arrays->arr_std_par) {
+        
+        free(arrays->arr_base);
+        free(arrays->arr_bit_seq);
+        free(arrays->arr_bit_par);
+        free(arrays->arr_std_seq);
+        free(arrays->arr_std_par);
+        
         perror("malloc failed");
-        return EXIT_FAILURE;
+        return -1;
     }
+    return 0;
+}
+
+void free_arrays(array_list *arrays) {
+    free(arrays->arr_base);
+    free(arrays->arr_bit_seq);
+    free(arrays->arr_bit_par);
+    free(arrays->arr_std_seq);
+    free(arrays->arr_std_par);
+}
+
+void refill_arrays(array_list arrays, int arr_size){
+    gen_rand_num(arrays.arr_base, arr_size);
+    for (size_t i = 0; i < arr_size; i++) {
+        arrays.arr_bit_seq[i] = arrays.arr_base[i];
+        arrays.arr_bit_par[i] = arrays.arr_base[i];
+        arrays.arr_std_seq[i] = arrays.arr_base[i];
+        arrays.arr_std_par[i] = arrays.arr_base[i];
+    }
+}
+
+int main() {
+    long size = 5000000;
+    array_list arrays;
+    if (create_arrays(&arrays, size) != 0) return EXIT_FAILURE;
 
     double total_bit_seq = 0.0;
     double total_bit_par = 0.0;
@@ -36,43 +80,35 @@ int main(void) {
     double start, end;
 
     for (int r = 0; r < RUNS; r++) {
-        gen_rand_num(arr_base, ARRAY_SIZE);
-
-        // Przygotowanie czystych kopii
-        for (size_t i = 0; i < ARRAY_SIZE; i++) {
-            arr_bit_seq[i] = arr_base[i];
-            arr_bit_par[i] = arr_base[i];
-            arr_std_seq[i] = arr_base[i];
-            arr_std_par[i] = arr_base[i];
-        }
+        refill_arrays(arrays, ARRAY_SIZE);
 
         // 1. Bitowy sekwencyjny
         start = now_sec();
-        qs_bin_single(arr_bit_seq, 0, ARRAY_SIZE - 1, (int)(sizeof(elem_t)*8 - 1));
+        qs_bin_single(arrays.arr_bit_seq, 0, ARRAY_SIZE - 1, (int)(sizeof(elem_t)*8 - 1));
         end = now_sec();
         total_bit_seq += (end - start);
-        if (!verify_sorted(arr_bit_seq, ARRAY_SIZE)) { fprintf(stderr,"Błąd bit_seq\n"); break; }
+        if (!verify_sorted(arrays.arr_bit_seq, ARRAY_SIZE)) { fprintf(stderr,"Błąd bit_seq\n"); break; }
 
         // 2. Bitowy równoległy
         start = now_sec();
-        qs_bin_parallel(arr_bit_par, ARRAY_SIZE, PARALLEL_THRESHOLD);
+        qs_bin_parallel(arrays.arr_bit_par, ARRAY_SIZE, PARALLEL_THRESHOLD);
         end = now_sec();
         total_bit_par += (end - start);
-        if (!verify_sorted(arr_bit_par, ARRAY_SIZE)) { fprintf(stderr,"Błąd bit_par\n"); break; }
+        if (!verify_sorted(arrays.arr_bit_par, ARRAY_SIZE)) { fprintf(stderr,"Błąd bit_par\n"); break; }
 
         // 3. Klasyczny sekwencyjny
         start = now_sec();
-        qs_std_single(arr_std_seq, 0, ARRAY_SIZE - 1);
+        qs_std_single(arrays.arr_std_seq, 0, ARRAY_SIZE - 1);
         end = now_sec();
         total_std_seq += (end - start);
-        if (!verify_sorted(arr_std_seq, ARRAY_SIZE)) { fprintf(stderr,"Błąd std_seq\n"); break; }
+        if (!verify_sorted(arrays.arr_std_seq, ARRAY_SIZE)) { fprintf(stderr,"Błąd std_seq\n"); break; }
 
         // 4. Klasyczny równoległy
         start = now_sec();
-        qs_std_parallel(arr_std_par, ARRAY_SIZE, PARALLEL_THRESHOLD);
+        qs_std_parallel(arrays.arr_std_par, ARRAY_SIZE, PARALLEL_THRESHOLD);
         end = now_sec();
         total_std_par += (end - start);
-        if (!verify_sorted(arr_std_par, ARRAY_SIZE)) { fprintf(stderr,"Błąd std_par\n"); break; }
+        if (!verify_sorted(arrays.arr_std_par, ARRAY_SIZE)) { fprintf(stderr,"Błąd std_par\n"); break; }
     }
 
     printf("============================================================\n");
@@ -87,7 +123,6 @@ int main(void) {
     printf("Przyspieszenie klasycznego: x%.2f\n", total_std_par / total_bit_par);
     printf("============================================================\n");
 
-    free(arr_base); free(arr_bit_seq); free(arr_bit_par);
-    free(arr_std_seq); free(arr_std_par);
+    free_arrays(&arrays);
     return 0;
 }
